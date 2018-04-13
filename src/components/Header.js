ae8f085570d51c2a9eb44e42f33ea574c1e16d7c
Donny Blaine <rykuno@gmail.com>
2018-04-13 18:42:49 -0500

    Converted all images to become local.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,9 @@
 import React from 'react';
+import hackathonBanner from '../images/hackathonBanner.gif';
 
 const Header = () => (
   <div>
-    <img
-      className="banner-gif"
-      src="https://cdn.discordapp.com/attachments/408739428236918784/421752700414328843/HackAThon-Purp.gif"
-      alt=""
-    />
+    <img className="banner-gif" src={hackathonBanner} alt="#" />
   </div>
 );
 
